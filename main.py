8489bf88d50aafb2c5a38750dd2907416b90fcd6
Saesenthessis66 <lucjangruby@gmail.com>
2024-06-05 20:49:58 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,79 @@
+import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import matplotlib.pyplot as plt
+
+currentPath = os.getcwd()
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    currentPath + '/train',
+    target_size=(256, 256),
+    batch_size=32,
+    class_mode='binary')
+
+validation_generator = validation_datagen.flow_from_directory(
+    currentPath + '/valid',
+    target_size=(256, 256),
+    batch_size=32,
+    class_mode='binary')
+
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(32, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dropout(0.5),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+# Evaluate the model on the validation data
+val_loss, val_acc = model.evaluate(validation_generator, steps=50)
+print(f"Validation loss: {val_loss}")
+print(f"Validation accuracy: {val_acc}")
+
+# Plot training & validation accuracy values
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
